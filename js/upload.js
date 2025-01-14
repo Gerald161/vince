35a413499c3f0ac75e680e05be81f25bb4f2508a
Gerald161 <56877559+Gerald161@users.noreply.github.com>
2025-01-14 11:34:48 +0000

    Add files via upload

---
+++ js/upload.js
@@ -0,0 +1,46 @@
+var extra_image_button = document.querySelector("#extra_image");
+
+var selected_images_container = document.querySelector(".selected_images_container");
+
+var all_extra_images = [];
+
+extra_image_button.addEventListener("change", e =>{
+	if(extra_image_button.files.length){
+		add_extra_image_to_list(extra_image_button.files[0]);
+	}
+});
+
+function add_extra_image_to_list(file){
+	if(file.type.startsWith("image/")){
+		const reader = new FileReader();
+
+		reader.readAsDataURL(file);
+
+		reader.onload = () => {
+			all_extra_images.push(reader.result);
+
+			updateAllExtraImages();
+		}
+	}
+}
+
+function updateAllExtraImages(){
+	selected_images_container.innerHTML = ``;
+
+	all_extra_images.forEach((extra_image, index)=>{
+		selected_images_container.innerHTML += `
+		<div class="selected_image">
+          <img src="${extra_image}">
+          <i data-index="${index}" class="fa-solid fa-xmark"></i>
+        </div>
+		` 
+	});
+
+	selected_images_container.querySelectorAll("i").forEach((cancel_button)=>{
+		cancel_button.addEventListener("click", (e)=>{
+			all_extra_images.splice(parseInt(cancel_button.dataset.index), 1);
+
+			updateAllExtraImages();
+		});
+	});
+}
